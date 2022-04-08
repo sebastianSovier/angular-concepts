85511c0f34aae24acd7128ab6a49d7c4cee0410f
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2022-04-08 14:29:36 -0400

    headers replace

--- src/app/mantenedor/mantenedor.service.ts
+++ src/app/mantenedor/mantenedor.service.ts
@@ -14,41 +14,35 @@ export class MantenedorService {
 
   ObtenerPaises(usuario:string) {
    //const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
+   //,{headers:headers}
     const result: Observable<any> = this.http.get(AppConfig.settings.UrlWebApi +'/Countries/TodosLosPaises?usuario='+usuario);
     return result;
   }
   IngresarPais(pais:any) {
-    const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
      const result: Observable<any> = this.http.post(AppConfig.settings.UrlWebApi +'/Countries/IngresarPais',pais );
      return result;
    }
   ModificarPais(pais:any) {
-    const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
      const result: Observable<any> = this.http.put(AppConfig.settings.UrlWebApi +'/Countries/ModificarPais',pais );
      return result;
    }
    EliminarPais(pais_id:string,usuario:string) {
-    const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
      const result: Observable<any> = this.http.delete(AppConfig.settings.UrlWebApi +'/Countries/EliminarPais?pais_id='+pais_id+"&usuario="+usuario);
      return result;
    }
    ObtenerCiudades(pais_id:string) {
-    const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
      const result: Observable<any> = this.http.get(AppConfig.settings.UrlWebApi +'/Ciudades/CiudadesPais?pais_id='+pais_id);
      return result;
    }
    IngresarCiudad(ciudad:any) {
-     const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
       const result: Observable<any> = this.http.post(AppConfig.settings.UrlWebApi +'/Ciudades/IngresarCiudad',ciudad );
       return result;
     }
    ModificarCiudad(ciudad:any) {
-     const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
       const result: Observable<any> = this.http.put(AppConfig.settings.UrlWebApi +'/Ciudades/ModificarCiudad',ciudad );
       return result;
     }
     EliminarCiudad(element:any) {
-     const headers = new HttpHeaders({'Authorization':'Bearer '+sessionStorage.getItem('token')});
       const result: Observable<any> = this.http.delete(AppConfig.settings.UrlWebApi +'/Ciudades/EliminarCiudad?ciudad_id='+element.ciudad_id+'&pais_id='+element.pais_id);
       return result;
     }
