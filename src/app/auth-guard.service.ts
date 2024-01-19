38712d8898f6e641d4135868975d175357bef70b
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-01-19 12:01:47 -0300

    angular 16

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
   constructor(public auth: AuthService, public router: Router) { }
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
