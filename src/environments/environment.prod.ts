2a26b876c202ec193636e60ce969a927b540e560
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-02-13 12:01:52 -0300

    gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  secretKey:"a-very-long-radonmly-generated-secret-key-that-cannot-be-guessed",
-  UrlWebApi: "http://localhost:3000",
-  encrypt:true
-};
